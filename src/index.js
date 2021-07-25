ae90fd3512721df18b10c10c01512a4e5ef87e4e
Vladimir35-Sashko <79024705+Vladimir35-Sashko@users.noreply.github.com>
2021-07-25 15:59:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import menuElement from './menu.json';
 import menuTemplate from './templates/templates.hbs';
 import './themes.js';
@@ -10,5 +11,5 @@ let menu = document.querySelector('.js-menu');
 function buildMenu(array) {
     const markup = array.map(post => menuTemplate(post)).join('');
     menu.insertAdjacentHTML('beforeend', markup);
-   }
+}
 buildMenu(menuElement);
